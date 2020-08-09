68031914082dccffc4600d8386c7ea4de2d38b46
Shirashoji <tkm.shira@gmail.com>
2020-08-09 09:56:17 +0900

    commit

--- tweet.py
+++ tweet.py
@@ -1,16 +1,4 @@
 import f_twitter
 
-text = "連続でツイート"#input("Tweetする文: ")
-f_twitter.tweet(text)
-
-text = "てすと"#input("Tweetする文: ")
-f_twitter.tweet(text)
-
-text = "その1"#input("Tweetする文: ")
-f_twitter.tweet(text)
-
-text = "その2"#input("Tweetする文: ")
-f_twitter.tweet(text)
-
-text = "テスト終了"#input("Tweetする文: ")
+#input("Tweetする文: ")
 f_twitter.tweet(text)
